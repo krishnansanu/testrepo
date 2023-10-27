aafff60b7015893e5ee3064fe64e94ed15be30b3
krishnansanu <krishboss11@gmail.com>
2023-10-28 10:54:31 +1100

    Update iics_helper.py

--- cicd/iics_helper.py
+++ cicd/iics_helper.py
@@ -29,7 +29,6 @@ def iics_logout(login_url, sessionId)
 
 
 #Invoking Function
-sessID=iics_login("https://dm-ap.informaticacloud.com/ma/api/v3/InternalLogin","
-Krishnan.Ravi.uat2","Simplya!@789")
+sessID=iics_login("https://dm-ap.informaticacloud.com/ma/api/v3/InternalLogin","Krishnan.Ravi.uat2","Simplya!@789")
 print("SessionID - " sessID)
 
