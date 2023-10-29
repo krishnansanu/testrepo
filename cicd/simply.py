d41326d56946e7091c0a40ac3000fa397903bfa0
krishnansanu <krishboss11@gmail.com>
2023-10-29 21:13:36 +1100

    Update simply.py

--- cicd/simply.py
+++ cicd/simply.py
@@ -34,9 +34,10 @@ def iics_logout(URL,sessionId):
 print("Reading command line values")
 print(sys.argv)
 
+VAR0="".join(sys.argv[0:])
 VAR1="".join(sys.argv[1:])
 VAR2="".join(sys.argv[2:])
-print(VAR1 + " - " + VAR2)
+print(VAR0 + " - "VAR1 + " - " + VAR2)
 
 
 
